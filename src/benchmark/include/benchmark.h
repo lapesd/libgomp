73435af956fed26e59cd90984a9d456f960c09a6
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-06-20 12:22:54 -0300

    iterations

--- src/benchmark/include/benchmark.h
+++ src/benchmark/include/benchmark.h
@@ -20,6 +20,11 @@
 #ifndef BENCHMARK_H_
 #define BENCHMARK_H_
 
+	/**
+	 * @brief Number of iterations.
+	 */
+	#define NITERATIONS 10
+
 	/* Forward definitions. */
 	extern void benchmark(const unsigned *, unsigned, unsigned, unsigned);
 
--- src/benchmark/main.c
+++ src/benchmark/main.c
@@ -278,7 +278,8 @@ int main(int argc, const const char **argv)
 	
 	tasks = create_tasks(args.pdfid, args.niterations, args.kernelid);
 
-	benchmark(tasks, args.niterations, args.nthreads, args.load);
+	for (i = 0; i < NITERATIONS; i++)
+		benchmark(tasks, args.niterations, args.nthreads, args.load);
 		
 	/* House keeping. */
 	free(tasks);
