1991b6ac7109b24b83da127653955b6678d7764b
fbroqued <francois.broquedis@gmail.com>
2018-01-15 11:40:58 +0100

    [examples] Fix akward indentation in  the binlpt-01.c file.

--- src/libgomp/examples/binlpt-01.c
+++ src/libgomp/examples/binlpt-01.c
@@ -5,16 +5,16 @@
 
 int main(void)
 {
-    #pragma omp parallel
+#pragma omp parallel
     {
-		unsigned workloads[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-		int id = omp_set_workload(workloads, 10);
-		omp_set_loop(id);
-		omp_set_schedule(omp_sched_binlpt, 0);
-		#pragma omp for schedule(runtime)
-		for (size_t i = 0; i < 10; i++) {
-	    	printf("Hello, world from thread %i!\n", omp_get_thread_num());
-		}
+        unsigned workloads[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        int id = omp_set_workload(workloads, 10);
+        omp_set_loop(id);
+        omp_set_schedule(omp_sched_binlpt, 0);
+#pragma omp for schedule(runtime)
+        for (size_t i = 0; i < 10; i++) {
+            printf("Hello, world from thread %i!\n", omp_get_thread_num());
+        }
     }
 
     return EXIT_SUCCESS;
