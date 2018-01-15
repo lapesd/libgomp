ebdc6dcaf9dd7d84a5de79023f593f09e134b7df
fbroqued <francois.broquedis@gmail.com>
2018-01-15 10:43:28 +0100

    [binlpt] New test: binlpt-01.c.

---
+++ src/libgomp/examples/binlpt-01.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <omp.h>
+
+int main(void)
+{
+    #pragma omp parallel
+    {
+		unsigned workloads[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+		int id = omp_set_workload(workloads, 10);
+		omp_set_loop(id);
+		omp_set_schedule(omp_sched_binlpt, 0);
+		#pragma omp for schedule(runtime)
+		for (size_t i = 0; i < 10; i++) {
+	    	printf("Hello, world from thread %i!\n", omp_get_thread_num());
+		}
+    }
+
+    return EXIT_SUCCESS;
+}
